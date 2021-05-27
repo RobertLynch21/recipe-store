714d19a5079a0103e4ef929755590475073b1d04
Robert Lynch <r.f.lynch@gmail.com>
2021-05-27 10:56:26 -0400

    Server finished

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-sessions');
+
+const app = express();
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
+
+
+app.use(express.json());
+
+// DB Connection
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then( db => {
+    app.set('db', db)
+    console.log('Connected to the DataBase')
+})
+
+//endpoints
+
+//Listen
+
+app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
