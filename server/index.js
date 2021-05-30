17a0fbf496385a93195c228774c91bce5f82deea
Robert Lynch <r.f.lynch@gmail.com>
2021-05-29 23:09:32 -0400

    updating db

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
 
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        cookie: { maxAge: 6000}
+    })
+)
+
 // DB Connection
 
 massive({
@@ -20,6 +29,16 @@ massive({
 })
 
 //endpoints
+// app.post('/api/auth/register', userCtrl.register);
+// app.post('/api/auth/login', userCtrl.login);
+// app.get('/api/auth/me', userCtrl.getUser);
+// app.post('/api/auth/logout', userCtrl.logout);
+
+// //Post Endpoints
+// app.get('/api/posts', postCtrl.readPosts);
+// app.post('/api/post', postCtrl.createPost);
+// app.get('/api/post/:id', postCtrl.readPost);
+// app.delete('/api/post/:id', postCtrl.deletePost)
 
 //Listen
 
