90f23a4825ff1f59b7d53e6af6156602a1d3bedc
RobertLynch21 <r.f.lynch@gmail.com>
2021-06-10 19:52:35 -0400

    added react reducers, and hooks

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,27 @@
-const Cart = () => {
+import {useEffect} from 'react'
+import axios from 'axios'
+import {useSelector, useDispatch, connect} from 'react-redux'
+import {setCart} from '../redux/cartReducer'
+
+const Cart = (props) => {
+    const {cart} = useSelector((store) => store.cartReducer)
+    
+
+    useEffect(() => {
+        axios.get('/api/cart')
+        .then((res) => {
+            console.log(res.data)
+        })
+    }, [])
     return(
+        <div>
         <h1>My cart page</h1>
+        </div>
     )
 }
 
 export default Cart
+
+// const mapStateToProps = (store) => store.cartReducer
+// export default connect(mapStateToProps)(cart)
+
