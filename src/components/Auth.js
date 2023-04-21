51ca182e0d800ab01ef89368ccf35e067f6662d3
RobertLynch21 <r.f.lynch@gmail.com>
2023-04-21 17:10:21 -0400

    Cleaned up auth code and added best practices

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import axios from 'axios';
 import {setUser} from '../redux/authReducer';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 const Auth = (props) => {
     const [email, setEmail] = useState('')
@@ -29,12 +30,17 @@ const Auth = (props) => {
             <img className='reg-img' src={'https://images.unsplash.com/photo-1578425570232-bf51f753dd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'} alt="chef" />
         <h1>Sign in</h1>
         <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
         <button onClick={handleLogin}>Login</button>
         <button onClick={handleRegister}>Register</button>
         </div>
     )
 }
+
+Auth.propTypes = {
+    setUser: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
+  };
 
 
 
